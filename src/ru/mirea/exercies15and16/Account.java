338badf82e978f93bc857bbcff5a929f547e546f
Northenfall <250322@mail.ru>
2020-11-16 23:07:52 +0300

    Final ex15_16

---
+++ src/ru/mirea/exercies15and16/Account.java
@@ -0,0 +1,9 @@
+package ru.mirea.exercies15and16;
+
+public class Account {
+    public int balance = 100;
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+}
---
+++ src/ru/mirea/exercies15and16/robot.java
@@ -0,0 +1,56 @@
+package ru.mirea.exercies15and16;
+
+public class robot {
+
+        public static void main(String[] args) {
+
+            Step commonResource= new Step();
+            System.out.println("Started ");
+
+            for (int i = 1; i < 3; i++){
+
+                Thread a = new Thread(new First(commonResource));
+
+                a.setName("Thead " + i );
+
+                a.start();
+            }
+        }
+    }
+
+    class Step {
+
+        boolean step = true;
+    }
+
+    class First implements Runnable{
+
+        Step res;
+
+        First(Step res){
+            this.res=res;
+        }
+        public void change (){
+            res.step = !res.step;
+        }
+        public void run(){
+
+            synchronized(res) {
+
+                try {
+                    if (res.step) {
+                        System.out.println("RIGHT");
+                    } else {
+                        System.out.println("LEFT");
+                    }
+                    change();
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+
+            }
+
+        }
+    }
+
+
