8214149dd72ea3b26afd35a3693ff059db9385d8
Northenfall <250322@mail.ru>
2020-11-29 21:43:19 +0300

    final ex19_20

---
+++ src/ru/mirea/exercies19and20/Main19_20.java
@@ -0,0 +1,14 @@
+package ru.mirea.exercies19and20;
+
+public class Main19_20 {
+    public static void main(String[] args) {
+
+        Generate generate = new Generate();
+        generate.beatifulplates();
+
+        /*Scanner scanner = new Scanner(System.in);
+        String in = scanner.nextLine();*/
+        generate.search("Т111ВУ64");
+
+    }
+}
